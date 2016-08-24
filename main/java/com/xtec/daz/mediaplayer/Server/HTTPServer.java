0889d49c5c9ea76447d5cbc6403ee459391faecf
Dariush Bahri <db819@york.ac.uk>
2016-08-24 12:23:52 +0100

    Can save index page data.

--- main/java/com/xtec/daz/mediaplayer/Server/HTTPServer.java
+++ main/java/com/xtec/daz/mediaplayer/Server/HTTPServer.java
@@ -5,9 +5,11 @@ package com.xtec.daz.mediaplayer.Server;
  */
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.webkit.WebView;
 
 import com.xtec.daz.mediaplayer.R;
@@ -39,80 +41,92 @@ public class HTTPServer extends NanoHTTPD {
             MIME_DEFAULT_BINARY = "application/octet-stream",
             MIME_XML = "text/xml";
 
+    // Stores all media player settings
+    public static final String PREFS_NAME = "MediaPlayerSettings";
+
+    SharedPreferences settings;
+    SharedPreferences.Editor editor;
+
     public HTTPServer(Context con) throws IOException {
         super(PORT);
         context = con;
         start();
+
+        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        editor = settings.edit();
+
+        // Add default unchangeable values TODO: change what these are depending on device
+        editor.putString("FWNum", "123456");
+        editor.commit();
     }
 
+    //TODO: add variables to string file. fix post http methods to save shit. test again
     @Override
     public Response serve(IHTTPSession session) {
         String uri = session.getUri();
+        Map<String, String> parms = session.getParms();
+
         if (uri.contains("x_sml.gif")) {
-                InputStream buffer = context.getResources().openRawResource(
-                        context.getResources().getIdentifier("x_sml",
-                                "raw", context.getPackageName()));
+            // Add logo
+            InputStream buffer = context.getResources().openRawResource(
+                    context.getResources().getIdentifier("x_sml",
+                            "raw", context.getPackageName()));
 
-                return new NanoHTTPD.Response(Response.Status.OK, MIME_GIF, buffer, 3958); // 3958 is the number of bytes logo is
-        }
+            return new NanoHTTPD.Response(Response.Status.OK, MIME_GIF, buffer, 3958); // 3958 is the number of bytes logo is
 
-        else if (uri.contains("index.html")) {
+        } else if (uri.contains("index.html")) {
+
+            if (parms.get("DevDesc") != null) {
+                // save device description
+                editor.putString("DevDesc", parms.get("DevDesc"));
+                editor.commit();
+            }
+
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("index",
                             "raw", context.getPackageName()));
 
             String msg = convertStreamToString(ins); // Convert input stream to a string
             msg = replaceTemplate(msg);
-
             return newFixedLengthResponse(msg);
 
-        }
-
-        else if (uri.contains("net.html")) {
+        } else if (uri.contains("net.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("net",
                             "raw", context.getPackageName()));
 
             String msg = convertStreamToString(ins); // Convert input stream to a string
             msg = replaceTemplate(msg);
-
             return newFixedLengthResponse(msg);
-        }
 
-        else if (uri.contains("din.html")) {
+        } else if (uri.contains("din.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("din",
                             "raw", context.getPackageName()));
 
             String msg = convertStreamToString(ins); // Convert input stream to a string
             msg = replaceTemplate(msg);
-
             return newFixedLengthResponse(msg);
-        }
 
-        else if (uri.contains("dout.html")) {
+        } else if (uri.contains("dout.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("dout",
                             "raw", context.getPackageName()));
 
             String msg = convertStreamToString(ins); // Convert input stream to a string
             msg = replaceTemplate(msg);
-
             return newFixedLengthResponse(msg);
-        }
 
-        else if (uri.contains("oth.html")) {
+        } else if (uri.contains("oth.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("oth",
                             "raw", context.getPackageName()));
 
             String msg = convertStreamToString(ins); // Convert input stream to a string
             msg = replaceTemplate(msg);
-
             return newFixedLengthResponse(msg);
-        }
 
-        else if (uri.contains("gen_error.html")) {
+        } else if (uri.contains("gen_error.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("gen_error",
                             "raw", context.getPackageName()));
@@ -121,9 +135,8 @@ public class HTTPServer extends NanoHTTPD {
             msg = replaceTemplate(msg);
 
             return newFixedLengthResponse(msg);
-        }
 
-        else if (uri.contains("netdata_error.html")) {
+        } else if (uri.contains("netdata_error.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("netdata_error",
                             "raw", context.getPackageName()));
@@ -132,9 +145,7 @@ public class HTTPServer extends NanoHTTPD {
             msg = replaceTemplate(msg);
 
             return newFixedLengthResponse(msg);
-        }
-
-        else if (uri.contains("save_error.html")) {
+        } else if (uri.contains("save_error.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("save_error",
                             "raw", context.getPackageName()));
@@ -143,9 +154,8 @@ public class HTTPServer extends NanoHTTPD {
             msg = replaceTemplate(msg);
 
             return newFixedLengthResponse(msg);
-        }
 
-        else if (uri.contains("submit_ok.html")) {
+        } else if (uri.contains("submit_ok.html")) {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("submit_ok",
                             "raw", context.getPackageName()));
@@ -154,9 +164,8 @@ public class HTTPServer extends NanoHTTPD {
             msg = replaceTemplate(msg);
 
             return newFixedLengthResponse(msg);
-        }
 
-        else {
+        } else {
             InputStream ins = context.getResources().openRawResource(
                     context.getResources().getIdentifier("index",
                             "raw", context.getPackageName()));
@@ -171,6 +180,7 @@ public class HTTPServer extends NanoHTTPD {
     /**
      * In the HTML code, there are variables between ~ which represents template HTML code or
      * stored values. This replaces the variable names with their values.
+     *
      * @param msg html code with ~
      * @return complete html code
      */
@@ -189,8 +199,7 @@ public class HTTPServer extends NanoHTTPD {
                                     "raw", context.getPackageName())); // Open file name
                     String fileHTML = convertStreamToString(fileIns);
                     msg = new StringBuilder(msg).replace(i, msg.indexOf('~', i + 1) + 1, fileHTML).toString(); // Insert html in place of variable
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     System.out.print("Error in replacing html code: " + e);
                     msg = new StringBuilder(msg).replace(i, msg.indexOf('~', i + 1) + 1, "").toString(); // Error. Put blank
                 }
@@ -198,11 +207,9 @@ public class HTTPServer extends NanoHTTPD {
             } else {
                 String value = msg.substring(i + 1, msg.indexOf('~', i + 1)); // Get variable name
                 try {
-                    String storedValue = (String) context.getResources().getText(
-                            context.getResources().getIdentifier(value, "string", context.getPackageName()));
+                    String storedValue = settings.getString(value, ""); // Get value from shared preferences
                     msg = new StringBuilder(msg).replace(i, msg.indexOf('~', i + 1) + 1, storedValue).toString();
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     System.out.print("Error in finding variable: " + e);
                     msg = new StringBuilder(msg).replace(i, msg.indexOf('~', i + 1) + 1, "").toString(); // Error. Put blank
 
@@ -220,6 +227,7 @@ public class HTTPServer extends NanoHTTPD {
      * Scanner iterates over tokens in the stream, and in this case we separate tokens using
      * "beginning of the input boundary" (\A) thus giving us only one token for the entire
      * contents of the stream.
+     *
      * @param is
      * @return
      */
